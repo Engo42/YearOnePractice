fed7dcf58cfd4e8cbe127de239c5c1d5af156c57
Anika1d <71558328+Anika1d@users.noreply.github.com>
2021-07-26 02:08:56 +1000

    Create BanditModel.js

---
+++ BanditModel.js
@@ -0,0 +1,21 @@
+class Bandit{
+    constructor() {///какие координаты у середины?
+       this.location=-1;
+       this.x;
+       this.y;
+
+    }
+    change(loc,i,j){
+        this.location=loc;
+        this.x=i;
+        this.y=j;
+
+    }
+    goHome(){
+        this.location=-1;
+        this.x;////координаты середины
+        this.y;
+
+    }
+
+}
