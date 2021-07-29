9cd6c048310e7fda258d6059ec94630f070c4e27
Engo42 <73175817+Engo42@users.noreply.github.com>
2021-07-29 18:21:26 +1000

    Доделал меню торговли

--- TradingModeUI.js
+++ TradingModeUI.js
@@ -1,5 +1,16 @@
 class TradingModeUI {
     constructor() {
+        this.img = new Array(5);
+        this.img[0] = new Image();
+        this.img[0].src = 'Sprites/Resources/wood.png';
+        this.img[1] = new Image();
+        this.img[1].src = 'Sprites/Resources/wool.png';
+        this.img[2] = new Image();
+        this.img[2].src = 'Sprites/Resources/corn.png';
+        this.img[3] = new Image();
+        this.img[3].src = 'Sprites/Resources/clay.png';
+        this.img[4] = new Image();
+        this.img[4].src = 'Sprites/Resources/iron.png';
         this.buttonsSellInc = new Array;
         this.buttonsSellDec = new Array;
         this.buttonsBuyInc = new Array;
@@ -16,8 +27,7 @@ class TradingModeUI {
             );
             this.buttonsSellDec[i] = new Button(60 + i * 70, 645, 20, 20, i, this,
                 function(id, parentUI) {
-                    if (parentUI.resourceSell[id] > 0)
-                        parentUI.resourceSell[id]--;
+                    parentUI.resourceSell[id]--;
                 }
             );
             this.buttonsBuyInc[i] = new Button(60 + i * 70, 700, 20, 20, i, this,
@@ -31,6 +41,15 @@ class TradingModeUI {
                 }
             );
         }
+        this.buttonConfirm = new Button(20, 800, 240, 60, i, this,
+            function(id, parentUI) {
+                for (var i = 0; i < 5; i++) {
+                    players[currentPlayer].resources[i] += parentUI.resourceBuy[i] - parentUI.resourceSell[i];
+                    parentUI.resourceSell[i] = 0;
+                    parentUI.resourceBuy[i] = 0;
+                }
+            }
+        )
     }
 
     frameAction() {
@@ -53,6 +72,10 @@ class TradingModeUI {
             else
                 this.buttonsBuyDec[i].active = true;
         }
+        if (arrSum(this.resourceBuy) * 2 !== arrSum(this.resourceSell) || arrSum(this.resourceBuy) === 0)
+            this.buttonConfirm.active = false;
+        else
+            this.buttonConfirm.active = true;
     }
 
     draw() {
@@ -63,6 +86,8 @@ class TradingModeUI {
         ctx.fillText('Продать', 10, 570);
         ctx.fillText('Купить', 10, 670);
         for (var i = 0; i < 5; i++) {
+            ctx.drawImage(this.img[i], 25 + i * 70, 615);
+            ctx.drawImage(this.img[i], 25 + i * 70, 715);
             drawColorBox(this.buttonsSellInc[i], '#444444', '#000088', '#4444FF', '#0000FF');
             drawColorBox(this.buttonsSellDec[i], '#444444', '#000088', '#4444FF', '#0000FF');
             drawColorBox(this.buttonsBuyInc[i], '#444444', '#000088', '#4444FF', '#0000FF');
@@ -70,9 +95,14 @@ class TradingModeUI {
             ctx.fillStyle = 'white';
             ctx.textBaseline = "middle";
             ctx.textAlign = "center";
-            ctx.fillText(this.resourceSell[i], 70 + i * 70, 620 + 14);
-            ctx.fillText(this.resourceBuy[i], 70 + i * 70, 720 + 14);
+            ctx.fillText(this.resourceSell[i], 70 + i * 70, 634);
+            ctx.fillText(this.resourceBuy[i], 70 + i * 70, 734);
         }
+        drawColorBox(this.buttonConfirm, '#444444', '#000088', '#4444FF', '#0000FF');
+        ctx.fillStyle = 'white';
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "left";
+        ctx.fillText('Заключить сделку', 30, 830);
     }
 
     deleteSelf() {
