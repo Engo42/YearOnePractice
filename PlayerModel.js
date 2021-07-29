4474bc01f33460c4844ef2ae0a6d0d6f10f2e742
Timofeev Timofey <timatimof2002@gmail.com>
2021-07-29 17:07:25 +1000

    Ничего не поменялось

--- PlayerModel.js
+++ PlayerModel.js
@@ -19,11 +19,11 @@ class Player {
         this.victoryPoints = 0;
         this.isLocalBot = false;
     }
-    
+
     startMove() {
         UI.dices.throwDices();
     }
-    
+
     harvest(level) {
         for (var i = 0; i < field.hexArray.length; i++) {
             if (field.hexArray[i].level === level) {
@@ -34,28 +34,28 @@ class Player {
             }
         }
     }
-    
+
     moveBandit() {
         var arr = new Array;
         var banditLocation;
-        for (var i = 0; i < field.hexArray.length; i++){
+        for (var i = 0; i < field.hexArray.length; i++) {
             if (field.hexArray[i].bandit !== 0)
                 field.hexArray[i].bandit = 0;
             else
                 arr.push(i);
         }
         field.hexArray[arr[Math.floor(Math.random() * arr.length)]].bandit = 1;
-        for (var i = 0; i < 4; i++){
+        for (var i = 0; i < 4; i++) {
             let res = players[i].resources;
             if (res[0] + res[1] + res[2] + res[3] + res[4] > 7) {
                 var a = new Array;
-                for (var j = 0; j < 5; j++){
-                    for (var k = 0; k < res[j]; k++){
+                for (var j = 0; j < 5; j++) {
+                    for (var k = 0; k < res[j]; k++) {
                         a.push(j);
                     }
                 }
                 shuffle(a);
-                for (var j = 0; j < a.length / 2; j++){
+                for (var j = 0; j < a.length / 2; j++) {
                     res[a[j]]--;
                 }
             }
